5116dd7e0c142688b551a117ed0150acb9e2f2c3
jiyu <pllpopogg@gmail.com>
2026-05-15 10:28:21 +0900

    3-2

---
+++ code/3-2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp_asc(const void * a, const void * b) {
+    return *(int*)a - *(int*)b;
+}
+
+int cmp_desc(const void* a, const void* b) {
+    return *(int*)b - *(int*)a;
+}
+
+
+void print_arr(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+
+int main() {
+    int arr[] = { 5, 2, 8, 1, 9, 3 };
+    int size = 6;
+
+    qsort(arr, size, sizeof(int), cmp_asc); //정렬할 배열 포인터, 원소개수, 원소크기, 비교함수포인터)
+    print_arr(arr, size);
+
+    qsort(arr, size, sizeof(int), cmp_desc);
+    print_arr(arr, size);
+
+    return 0;
+}
